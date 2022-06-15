8cbb8afbdfe42d5145e09dc1834facb329e682fb
ZuhairN <zuhairnawasreh@gmail.com>
2022-06-15 20:26:24 +0300

    add mobile homepage partII

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menu = document.querySelector('.nav__menu');
+const hList = document.querySelector('.header__list');
+
+menu.addEventListener('click', () => {
+  menu.parentElement.parentElement.classList.toggle('active');
+  if (menu.getAttribute('src').indexOf('hamb') > -1) {
+    menu.setAttribute('src', './images/icon-close.svg');
+  } else {
+    menu.setAttribute('src', './images/icon-hamburger.svg');
+  }
+});
