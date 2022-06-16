852ed72eca5bbf0d43883adef56768340112a389
ZuhairN <zuhairnawasreh@gmail.com>
2022-06-16 23:53:13 +0300

    add mobile homepage partIII

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const menu = document.querySelector('.nav__menu');
-const hList = document.querySelector('.header__list');
+const tabs = document.querySelector('.features__tab');
+const tabsContent = document.querySelectorAll('.illustration__tab');
+const QList = document.querySelector('.FAQ__list');
+const questions = document.querySelectorAll('.FAQ__tab');
 
 menu.addEventListener('click', () => {
   menu.parentElement.parentElement.classList.toggle('active');
@@ -9,5 +12,31 @@ menu.addEventListener('click', () => {
     menu.setAttribute('src', './images/icon-close.svg');
   } else {
     menu.setAttribute('src', './images/icon-hamburger.svg');
+  }
+});
+
+tabs.addEventListener('click', (e) => {
+  e.preventDefault();
+  const tab = e.target;
+  let j;
+  if (tab.tagName === 'A') {
+    for (let i = 0; i < 3; i++) {
+      if (tabs.children[i].getAttribute('class').indexOf('active') > -1) {
+        tabs.children[i].classList.remove('active');
+        tabsContent[i].classList.remove('active');
+      }
+      if (tab.parentElement.getAttribute('class').indexOf(i + 1) > -1) {
+        j = i;
+      }
+    }
+    tab.parentElement.classList.add('active');
+    tabsContent[j].classList.add('active');
+  }
+});
+
+QList.addEventListener('click', (e) => {
+  const q = e.target;
+  if (q.getAttribute('class').indexOf('question') > -1) {
+    q.parentElement.classList.toggle('active');
   }
 });
