4b9741dc73496b00151cc145cbbe990468454d98
ZuhairN <zuhairnawasreh@gmail.com>
2022-06-19 23:56:19 +0300

    refactoring and implementing SASS into code part III

--- js/app.js
+++ js/app.js
@@ -5,10 +5,9 @@ const nav=document.querySelector('.nav');
 const tabs = document.querySelector('.features__tab');
 const tabsContent = document.querySelectorAll('.illustration__tab');
 const QList = document.querySelector('.FAQ__list');
-const questions = document.querySelectorAll('.FAQ__tab');
 
 menu.addEventListener('click', () => {
-  nav.classList.toggle('active');
+  nav.classList.toggle('nav--active');
   if (menu.getAttribute('src').indexOf('hamb') > -1) {
     menu.setAttribute('src', './images/icon-close.svg');
   } else {
@@ -23,21 +22,21 @@ tabs.addEventListener('click', (e) => {
   if (tab.tagName === 'A') {
     for (let i = 0; i < 3; i++) {
       if (tabs.children[i].getAttribute('class').indexOf('active') > -1) {
-        tabs.children[i].classList.remove('active');
-        tabsContent[i].classList.remove('active');
+        tabs.children[i].classList.remove('features__list--active');
+        tabsContent[i].classList.remove('illustration__tab--active');
       }
       if (tab.parentElement.getAttribute('class').indexOf(i + 1) > -1) {
         j = i;
       }
     }
-    tab.parentElement.classList.add('active');
-    tabsContent[j].classList.add('active');
+    tab.parentElement.classList.add('features__list--active');
+    tabsContent[j].classList.add('illustration__tab--active');
   }
 });
 
 QList.addEventListener('click', (e) => {
   const q = e.target;
   if (q.getAttribute('class').indexOf('question') > -1) {
-    q.parentElement.classList.toggle('active');
+    q.parentElement.classList.toggle('tab--active');
   }
 });
